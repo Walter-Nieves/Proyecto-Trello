ea09b41f26f18709a004ddab0ea83d86d7c7e66a
Walter-Nieves <nievesarrietawalter@gmail.com>
2025-09-06 23:16:22 -0500

    Update sistema_notas.js

--- utilidades/sistema_notas.js
+++ utilidades/sistema_notas.js
@@ -121,7 +121,7 @@ function crearHTMLNota(...notas) {
          <div class="titulo-nota">
              <h3>${titulo}</h3>
              <div>
-                 <img class="menu-nota" src="../imagen/three-dots-vertical.svg" alt="Options">
+                 <img class="menu-nota" src="./imagen/three-dots-vertical.svg" alt="Options">
                  <ul class="ul-nota">
                      <li class="btn-modificar-nota">Modificar</li>
                      <li class="btn-borrar-nota" >Borrar</li>
